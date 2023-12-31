3de391e75432062519c4469d333751532f490274
Yiğit Ata Öztürk <yigit.ata.ozturk@outlook.com>
2024-01-01 01:52:35 +0300

    GCC Error Handled

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import store from './store'
 
 loadFonts()
 
-createApp(App)
+createApp(App).use(store)
   .use(router)
   .use(vuetify)
   .mount('#app')
